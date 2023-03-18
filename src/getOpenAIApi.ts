5a8691331b6eebe7a983082b030eb97a058f5046
Matthew Herod <matthew.herod@myunidays.com>
2023-03-18 01:48:59 +0000

    quick and simple

---
+++ src/getOpenAIApi.ts
@@ -0,0 +1,26 @@
+import { Configuration, OpenAIApi } from "openai";
+import path from "path";
+import { existsSync } from "fs";
+import { readJson } from "./readJson";
+import { ConfigurationParameters } from "openai/configuration";
+
+export async function getOpenAIApi(): Promise<OpenAIApi> {
+  const homeDir = process.env.HOME;
+  if (!homeDir) {
+    throw new Error("No home dir");
+  }
+  const keyPath = path.join(homeDir, ".openai-config.json");
+  if (!existsSync(keyPath)) {
+    throw new Error(
+      "No key. Create a file at " +
+        keyPath +
+        " with your OpenAI key like this: " +
+        JSON.stringify({ apiKey: "YOUR_KEY_HERE" }) +
+        " and try again."
+    );
+  }
+  const configParams = await readJson(keyPath);
+  const openAiConfig = configParams as ConfigurationParameters;
+  const configuration = new Configuration(openAiConfig);
+  return new OpenAIApi(configuration);
+}
--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,64 @@
 "use strict";
 
 import consola from "consola";
-import { Command, Option } from "commander";
+import { Command } from "commander";
 import { name, version } from "../package.json";
+import { openAIApiPromise } from "./whisper";
+import destr from "destr";
 
-new Command()
-  .name(name)
-  .version(version, "-v, --version", "output the current version")
-  .addOption(
-    new Option("-i, --input [input]", "set input").default("default")
-    //
-  )
-  .parseAsync(process.argv)
-  .then(
-    (command) => {
-      consola.info("Command:", command);
-    },
-    (err) => {
-      consola.error(err);
-      process.exit(1);
-    }
-    //
-  );
+async function main() {
+  const defaultSystemMessage = "Provide concise answers to questions.";
+  const defaultPrompt = "Hello world!";
+  const command = await new Command()
+    .name(name)
+    .version(version, "-v, --version", "output the current version")
+    .option("-s, --system [system]", "system prompt", defaultSystemMessage)
+    .option(
+      "-p, --prompt [prompt]",
+      "user prompt",
+      defaultPrompt
+    )
+    .option(
+      "-t, --temperature [temperature]",
+      "Temperature for prompt",
+      "0.5"
+      //
+    )
+    .option(
+      "-m, --max-tokens [max_tokens]",
+      "Max tokens for prompt",
+      "100"
+      //
+    )
+    .parseAsync(process.argv);
+
+  const options = command.opts();
+  const openAI = await openAIApiPromise;
+  const completion = await openAI.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: options.system || defaultSystemMessage
+      },
+      {
+        role: "user",
+        content: options.prompt
+      }
+    ],
+    temperature: destr(options.temperature),
+    max_tokens: destr(options.max_tokens) || 100
+  });
+
+  const text = completion.data.choices //
+    .map((choice) => choice.message) //
+    .map((message) => message.content.replace(/\s+/g, " ").trim())
+    .join();
+
+  console.log(text);
+}
+
+main().catch((error) => {
+  consola.error(error);
+  process.exit(1);
+});
